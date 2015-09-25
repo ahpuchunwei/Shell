ce32722f9af30701be84dcda2a6479f78a2ecf0e
蒲春伟 <ahpuchunwei@163.com>
2015-09-25 22:01:05 +0800

    Update shell1_1.sh

--- shell1_1.sh
+++ shell1_1.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
-#�Ա�����ֵ
-himi="Hello,Shell!" #�Ⱥ����߾������пո�Ĵ���
+#对变量赋值
+himi="Hello,Shell!" #等号两边均不能有空格的存在
 echo "A is:"
 echo $himi
 _int=19
 _tot=$_int+89
-echo $_tot #shellĬ�����ַ���
-_tot2=$[$_int+89] #�������ڵı���ʽ��Ϊ��ѧ�����ȼ����������,C shellֻ֧������������
+echo $_tot #shell默认是字符串
+_tot2=$[$_int+89] #中括号内的表达式作为数学运算先计算结果再输出,C shell只支持整数的运算
 echo $_tot2
-#shell�����������������+ - / * % << >>
+#shell常见的算术运算符有+ - / * % << >>
 var='expr $_int + 10'
 echo $var
-ls #�г���ǰĿ¼���ļ�
+ls #列出当前目录和文件
 echo "create puchunwei directory"
-mkdir puchunwei #����Ŀ�
-sudo ls #��ʱ��ù���ԱȨ����ʾ��ǰ�ļ����е�Ŀ¼���ļ�
-rmdir puchunwei #ɾ��Ŀ¼
+mkdir puchunwei #创建目录
+sudo ls #临时获得管理员权限显示当前文件夹中的目录和文件
+echo "list all directory"
+rmdir puchunwei #删除目录
+
 
 
