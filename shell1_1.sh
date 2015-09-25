b89d195c004ce7651405b680e36104be68f28fab
puchunwei <puchunwei@weidian.com>
2015-09-25 22:25:54 +0800

    update

--- shell1_1.sh
+++ shell1_1.sh
@@ -17,6 +17,16 @@ mkdir puchunwei #创建目录
 echo "list all directory"
 sudo ls #临时获得管理员权限显示当前文件夹中的目录和文件
 rmdir puchunwei #删除目录
+for((i=0;i<10;i++)); do
+	echo $i
+	touch test_$i.txt
+	rm test_$i.txt
+done
+ls -l #列出当前目录下的文件以及对应的权限
+name='puchunwei' #单引号里面的任何字符都会按照原样输出，单引号中的变量是无效的，单引号好不能出现单引号
+my_name="Hello,I am $_tot2" #双引号中可以有变量，可以出现转义字符
+echo $my_name
+
 
 
 
